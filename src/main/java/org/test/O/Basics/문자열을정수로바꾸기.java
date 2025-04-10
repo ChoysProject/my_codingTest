1294362afcccd1a05e5d2c57532127b9c6cf8bf0
choys <charm4860as@gmail.com>
2025-04-10 12:26:35 +0900

    1초 컷

---
+++ src/main/java/org/test/O/Basics/문자열을정수로바꾸기.java
@@ -0,0 +1,13 @@
+package org.test.O.Basics;
+
+public class 문자열을정수로바꾸기 {
+
+//    숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+//    n_str	result
+//      "10"	10
+//      "8542"	8542
+    public int solution(String n_str){
+        return Integer.parseInt(n_str);
+    }
+}
