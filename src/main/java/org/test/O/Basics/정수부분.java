4af4541e26ef3bfad945199d527c29e6964af104
choys <charm4860as@gmail.com>
2025-04-23 12:33:21 +0900

    점심 문제 완료

---
+++ src/main/java/org/test/O/Basics/정수부분.java
@@ -0,0 +1,16 @@
+package org.test.O.Basics;
+
+public class 정수부분 {
+
+//    실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+
+
+//    flo	result
+//1.42	1
+//        69.32	69
+
+    public int solution(double flo) {
+        return (int) flo;
+    }
+
+}
