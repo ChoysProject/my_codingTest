cf5ac608ee83e5dc11acad02731559b75a2857f7
choys <charm4860as@gmail.com>
2025-05-14 18:22:01 +0900

    삼각형 완료~

---
+++ src/main/java/org/test/O/Basics/삼각형의완벽한조건.java
@@ -0,0 +1,52 @@
+package org.test.O.Basics;
+
+import java.util.Arrays;
+
+public class 삼각형의완벽한조건 {
+
+//    선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+//
+//    가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+//    삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
+
+//    sides	result
+//[1, 2, 3]	2
+//        [3, 6, 2]	2
+//        [199, 72, 222]	1
+
+
+    public int solution(int[] sides){
+
+        Arrays.sort(sides);
+        return sides[0]+sides[1] <= sides[2] ? 2 : 1;
+
+    }
+
+
+    // 나의 좋지않은 코드
+
+//    public int solution(int[] sides) {
+//
+//        int goValue = 0;
+//        int juValue = 0;
+//
+//        for (int value : sides){
+//            if (goValue < value){
+//                goValue = value;
+//                juValue += value;
+//            }else {
+//                juValue += value;
+//            }
+//        }
+//
+//        juValue -= goValue;
+//
+//
+//        if (goValue >= juValue){
+//            return 2;
+//        }else {
+//            return 1;
+//        }
+//
+//    }
+}
